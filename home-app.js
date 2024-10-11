f20daf16f966fec1804fe45720b550f43359b777
jibinmatt <jibinjtomas@gmail.com>
2024-10-11 13:11:50 +0530

    completed loading books into home page

--- home-app.js
+++ home-app.js
@@ -17,15 +17,64 @@
 // 	book.childNodes[3].textContent = data[book.getAttribute('data-value')].title
 // })
 
+// import data from "./compact_cleaned_data.json"
 
 const booksView = document.querySelector(".books-view");
-const numOfBooksLoaded = 50;
+let numOfBooksLoaded = 50;
+dataFetch()
 
-for(let i = 0; i < numOfBooksLoaded; i++) {
-  const book = document.createElement("div");
-  book.id = "book";
-  book.className = "book";
-  booksView.appendChild(book);
+function dataFetch() {
+  fetch('/compact_cleaned_data.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    populateBooks(numOfBooksLoaded, data)
+    populateBookCovers(data)
+  });
 }
 
+function populateBooks(numOfBooksLoaded, data) {
+  for(let i = 0; i < numOfBooksLoaded; i++) {
+    const book = document.createElement("div");
+    const bookCover = document.createElement("img");
+    const bookLink = document.createElement("a");
+    
+    book.id = "book";
+    book.className = "book";
+    booksView.appendChild(book);
 
+    bookLink.href = `book-details.html?id=${data[i]["id"]}`
+    bookLink.target = "_blank"
+    book.appendChild(bookLink)
+
+    bookCover.alt = "Book Cover"
+    bookCover.loading = "lazy"
+    bookCover.className = "book-cover"
+    bookCover.id = "book-cover"
+    bookLink.appendChild(bookCover)
+  }
+}
+
+function populateBookCovers(data) {
+  const bookCovers = document.querySelectorAll(".book-cover")
+  bookCovers.forEach((bookCover, index) => {
+    bookCover.src = data[index]["cover_link"];
+    bookCover.alt = data[index]["title"]
+  });
+}
+
+function isAtBottom() {
+  const scrollTop = window.scrollY || window.pageYOffset;
+  const documentHeight = document.documentElement.scrollHeight;
+  const viewportHeight = window.innerHeight;
+  return scrollTop + viewportHeight >= documentHeight - 1;
+}
+
+window.addEventListener('scroll', () => {
+  if (isAtBottom()) {
+    // Increment the variable when the bottom is reached
+    numOfBooksLoaded += 20;
+    console.log(numOfBooksLoaded)  
+    dataFetch()
+  }
+});
