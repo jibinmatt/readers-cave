4e85ca957a3dfa21ccc1ee3dd7e08883577a8946
jibinmatt <jibinjtomas@gmail.com>
2024-10-16 23:37:51 +0530

    updated spotlight info to be randomly generated - removed duplicates from recommended books in json - updated spotlight css on home page - added link to author page in book details - changed grid view in recommended books

--- home-app.js
+++ home-app.js
@@ -1,50 +1,55 @@
 const booksView = document.querySelector(".books-view");
-let numOfBooksLoaded = 50;
+let numOfBooksLoaded = 0;
 dataFetch()
 
 function dataFetch() {
   fetch('/compact_cleaned_data.json')
   .then(response => response.json())
   .then(data => {
-    console.log(data)
     populateBooks(numOfBooksLoaded, data)
-    populateBookCovers(data)
   });
 }
 
 function populateBooks(numOfBooksLoaded, data) {
-  for(let i = 0; i < numOfBooksLoaded; i++) {
+  const maxBooks = Math.min(data.length, numOfBooksLoaded+50);
+  const spBookCover = document.querySelector(".spotlight-book-cover")
+  const spBookTitle = document.querySelector(".spotlight-book-title")
+  const spBookAuthor = document.querySelector(".spotlight-book-author")
+  const spBookDesc = document.querySelector(".spotlight-book-desc")
+  const spBookLink = document.querySelector(".spotlight-book-link")
+  
+  for (let i = numOfBooksLoaded; i < maxBooks; i++) {
     const book = document.createElement("div");
     const bookCover = document.createElement("img");
     const bookLink = document.createElement("a");
     
-    book.id = "book";
-    book.className = "book";
-    booksView.appendChild(book);
+    book.className = "book"
+    bookLink.href = `book-details.html?id=${data[i].id}`;
+    bookLink.target = "_blank";
+    bookCover.alt = "Book Cover";
+    bookCover.loading = "lazy";
+    bookCover.className = "book-cover";
 
-    bookLink.href = `book-details.html?id=${data[i]["id"]}`
-    bookLink.target = "_blank"
-    book.appendChild(bookLink)
-
-    bookCover.alt = "Book Cover"
-    bookCover.loading = "lazy"
-    bookCover.className = "book-cover"
-    bookCover.id = "book-cover"
-    bookLink.appendChild(bookCover)
-  }
-}
-
-function populateBookCovers(data) {
-  const bookCovers = document.querySelectorAll(".book-cover")
-  bookCovers.forEach((bookCover, index) => {
-    if (data[index]["cover_link"]) {
-      bookCover.src = data[index]["cover_link"];
-      bookCover.alt = data[index]["title"]
+    if (data[i]["cover_link"]) {
+      bookCover.src = data[i]["cover_link"];
+      bookCover.alt = data[i]["title"]
     } else {
-      bookCover.src = `https://placehold.co/224x336/B9B9B9/575757/?text=${data[index]["title"]}`
-      bookCover.alt = data[index]["title"]
+      bookCover.src = `https://placehold.co/224x336/B9B9B9/575757/?text=${data[i]["title"]}`
+      bookCover.alt = data[i]["title"]
     }
-  });
+
+    bookLink.appendChild(bookCover);
+    book.appendChild(bookLink);
+    booksView.appendChild(book);
+  }
+  let rI = Math.floor(Math.random() * maxBooks)
+  console.log(rI)
+  spBookTitle.textContent = data[rI]["title"]
+  spBookAuthor.textContent = data[rI]["author"]
+  spBookDesc.textContent = data[rI]["description"]
+  spBookCover.src = data[rI]["cover_link"]
+  spBookLink.href = `book-details.html?id=${data[rI].id}`
+  spBookLink.target = "_blank"
 }
 
 function isAtBottom() {
@@ -57,7 +62,7 @@ function isAtBottom() {
 window.addEventListener('scroll', () => {
   if (isAtBottom()) {
     // Increment the variable when the bottom is reached
-    numOfBooksLoaded += 20;
+    numOfBooksLoaded += 50;
     console.log(numOfBooksLoaded)  
     dataFetch()
   }
